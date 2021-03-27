45348dbe9656c0c3268948056d5ec758c70aaf51
thanon <thanon.von@gmail.com>
2021-03-27 16:17:18 +0700

    marketplace page

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Page = () => {
   return <div className="flex flex-col items-center justify-center bg-gray-300">
     <div>
       <img className="logo-header my-4" src="https://firebasestorage.googleapis.com/v0/b/nftpass-6056c.appspot.com/o/NFTpass.svg?alt=media&token=624e343b-d138-4253-893d-e0a8bb39a4f8" /></div>
-    <div className=" w-full md:w-1/2 bg-white p-6 text-center text-2xl style-box-primaey rounded-none flex flex-col bg-pattern">
+    <div className=" w-full md:w-1/2 bg-white p-6 text-center text-2xl style-box-primary rounded-none flex flex-col bg-pattern">
       <div className="flex flex-col md:flex-row mb-8">
 
         <img className="profile-image" src="https://images.barrons.com/im-310790?width=620&size=0.7498535442296427" alt="" />
@@ -38,7 +38,7 @@ const Page = () => {
       </div>
       <span className="text-left">Works(4)</span>
       <div className="grid grid-cols-2 gap-4 my-4 mb-8">
-        <a onClick={() => Router.push('/next_page')} className="style-box-primaey artwork-card flex-col text-left">
+        <a onClick={() => Router.push('/next_page')} className="style-box-primary artwork-card flex-col text-left">
           <img className="m-auto block thumbnail-work" src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/ce0cbc73949311.5c1adc41ccdcd.jpg" alt="" />
           <div className="mt-2">
             <span className="text-sm	block text-blue-600">Current Owner: Thanon</span>
@@ -48,7 +48,7 @@ const Page = () => {
             <span className="text-sm	block text-gray-500	">Blockchain: Ethereum</span>
           </div>
         </a>
-        <a className="style-box-primaey artwork-card flex-col text-left">
+        <a className="style-box-primary artwork-card flex-col text-left">
           <img className="m-auto block thumbnail-work" src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/cfe1cf73949311.5c1adc41c9b7b.jpg" alt="" />
           <div className="mt-2">
             <span className="text-sm	block text-blue-600">Current Owner: Thanon</span>
@@ -58,7 +58,7 @@ const Page = () => {
             <span className="text-sm	block text-gray-500	">Blockchain: Ethereum</span>
           </div>
         </a>
-        <a className="style-box-primaey artwork-card flex-col text-left">
+        <a className="style-box-primary artwork-card flex-col text-left">
           <img className="m-auto block thumbnail-work" src="https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/69caa773949311.5c1adc41c9f84.jpg" alt="" />
           <div className="mt-2">
             <span className="text-sm	block text-blue-600">Current Owner: Thanon</span>
@@ -68,7 +68,7 @@ const Page = () => {
             <span className="text-sm	block text-gray-500	">Blockchain: Ethereum</span>
           </div>
         </a>
-        <a className="style-box-primaey artwork-card flex-col text-left">
+        <a className="style-box-primary artwork-card flex-col text-left">
           <img className="m-auto block thumbnail-work" src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/9660c273949311.5c1adc41cc287.jpg" alt="" />
           <div className="mt-2">
             <span className="text-sm	block text-blue-600">Current Owner: Thanon</span>
