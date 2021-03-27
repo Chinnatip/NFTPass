80195079a3e084b55791785ae3da86910479f568
Chinnatip Taemkaeo <chin@kohlife.com>
2021-03-27 21:35:03 +0700

    fix collection page

--- interfaces/opensea.tsx
+++ interfaces/opensea.tsx
@@ -1,9 +1,4 @@
-import { useRouter } from 'next/router'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-
-
-type OpenseaCollection = {
+export interface OpenseaData {
   address: string
   asset_contract_type: string
   buyer_fee_basis_points: number
@@ -62,7 +57,66 @@ type OpenseaCollection = {
   total_supply: string
 }
 
-type OpenseaThing = {
+export interface OpenseaCollection  {
+  address: string
+  asset_contract_type: string
+  buyer_fee_basis_points: number
+  collection: {
+    banner_image_url: string
+    chat_url?: string
+    created_date: string
+    default_to_fiat: boolean
+    description: string
+    dev_buyer_fee_basis_points: string
+    dev_seller_fee_basis_points: string
+    discord_url?: string
+    display_data: {
+      card_display_style: string
+    }
+    external_url: string
+    featured: boolean
+    featured_image_url: string
+    hidden: boolean
+    image_url: string
+    instagram_username?: string
+    is_subject_to_whitelist: boolean
+    large_image_url: string
+    medium_username?: string
+    name: string
+    only_proxied_transfers: boolean
+    opensea_buyer_fee_basis_points: string
+    opensea_seller_fee_basis_points: string
+    payout_address: string
+    require_email: boolean
+    safelist_request_status: string
+    short_description?: string
+    slug: string
+    telegram_url?: string
+    twitter_username?: string
+    wiki_url?: string
+  }
+  created_date: string
+  default_to_fiat: boolean
+  description: string
+  dev_buyer_fee_basis_points: number
+  dev_seller_fee_basis_points: number
+  external_link: string
+  image_url: string
+  name: string
+  nft_version: string
+  only_proxied_transfers: boolean
+  opensea_buyer_fee_basis_points: number
+  opensea_seller_fee_basis_points: number
+  opensea_version?: string
+  owner?: string
+  payout_address: string
+  schema_name: string
+  seller_fee_basis_points: number
+  symbol: string
+  total_supply: string
+}
+
+export interface OpenseaThing  {
   banner_image_url: string
   chat_url?: string
   created_date: string
@@ -113,11 +167,11 @@ type OpenseaThing = {
   wiki_url: string
 }
 
-type OpenSeaNFT = {
+export interface OpenSeaNFT {
   assets: NFTAsset[]
 }
 
-type NFTAsset = {
+export interface NFTAsset {
   "id": number
   "token_id": string
   "num_sales": number
@@ -266,75 +320,3 @@ type NFTAsset = {
   "transfer_fee_payment_token"?: string
   "transfer_fee"?: string
 }
-
-const Page = () => {
-  const [creator, setCreator] = useState<OpenseaCollection>({})
-  const [creatorThings, setCreatorCurate] = useState<OpenseaThing[]>([])
-  const [creatorNFT, setcreatorNFT] = useState<NFTAsset[]>([])
-  useEffect(() => {
-    let assets : NFTAsset[] = []
-    axios.get('https://api.opensea.io/api/v1/asset_contract/0x12f28e2106ce8fd8464885b80ea865e98b465149').then(res => {
-      const creatorData : OpenseaCollection = res.data
-      setCreator(creatorData)
-      axios.get('https://api.opensea.io/api/v1/collections?asset_owner=0xc6b0562605d35ee710138402b878ffe6f2e23807&offset=0&limit=300').then(res => {
-        const thingsData : OpenseaThing[] = res.data
-        setCreatorCurate(thingsData)
-        axios.get('https://api.opensea.io/api/v1/assets?asset_contract_address=0x12f28e2106ce8fd8464885b80ea865e98b465149&order_direction=desc&offset=0&limit=50').then(res => {
-          const nfts : OpenSeaNFT = res.data
-          const nft = nfts.assets
-          assets.push(...nft)
-          axios.get('https://api.opensea.io/api/v1/assets?asset_contract_address=0x12f28e2106ce8fd8464885b80ea865e98b465149&order_direction=desc&offset=50&limit=50').then(res => {
-            const nfts : OpenSeaNFT = res.data
-            const nft = nfts.assets
-            assets.push(...assets,...nft)
-            // console.log(assets.length)
-            setcreatorNFT(nft.filter(thing => thing?.last_sale != undefined).sort((a,b) => parseFloat(b.last_sale.payment_token?.usd_price) - parseFloat(a.last_sale.payment_token.usd_price)))
-          })
-        })
-      })
-    })
-  },[])
-  useEffect(() => { }, [])
-  const Router = useRouter()
-  return <div className="flex flex-col items-center justify-center bg-gray-300">
-    <div><img className="logo-header my-4" src="https://firebasestorage.googleapis.com/v0/b/nftpass-6056c.appspot.com/o/NFTpass.svg?alt=media&token=624e343b-d138-4253-893d-e0a8bb39a4f8" /></div>
-    <div className=" w-full md:w-1/2 bg-white p-6 text-center text-2xl style-box-primary rounded-none flex flex-col bg-pattern">
-      <div className="flex flex-col mb-8">
-        <a onClick={() => Router.push('/')} className="text-left mb-2"> ← Back </a>
-        <img className="block w-full" src={creatorNFT[2]?.image_original_url} alt="" />
-        <div className="py-8 flex flex-col text-left">
-          <div className="mt-2">
-            <span className="text-sm	block text-blue-600">Name: {creatorNFT[2]?.name}</span>
-            <span className="text-sm	block">Token ID: {creatorNFT[2]?.token_id}</span>
-            <span className="text-sm	block text-gray-700	">Token ID: </span>
-            <span className="text-sm	block text-gray-700	">Description</span>
-          </div>
-        </div>
-      </div>
-    
-    <span className="text-left">Exhibition</span>
-    <div className="flex flex-col mb-8">
-    <a href={creator?.external_link} className="text-black bg-white rounded-none px-4 py-2 mt-4 border-gray-900 border-2"><img width="25" height="25" className="inline" src="https://res.cloudinary.com/nifty-gateway/image/upload/q_auto:good,w_500/v1576344316/nifty-builder-images/kyhclu5quebqm4sit0he.png" /> <span>Niftygateway</span></a>
-    </div>
-    <span className="text-left">Same Collection(4)</span>
-    <div className="">
-    {creatorNFT.map(thing => {
-      return <div className="m-auto mb-6 flex  bg-white w-full border-2 border-black">
-        <img src={thing?.image_thumbnail_url} className="h-full" alt=""/>
-        <div className="flex-grow ml-4 text-left text-base	p-2 ">
-          <p className="block">{thing?.name}</p>
-          <p className="block">current owner: {thing?.owner?.user?.username}</p>
-          <p className="block">
-            Sale for {thing.last_sale.quantity} times | { Math.floor(parseFloat(thing.last_sale.payment_token.usd_price))} usd
-          </p>
-          <a href={thing?.permalink} className="inline-block border-2 border-black px-4 py-1 bg-white">Link</a>
-        </div>
-
-      </div>
-    })}
-    </div>
-  </div>
-  </div>
-}
-
-export default Page
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,67 +1,9 @@
 import { useRouter } from 'next/router'
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
-import Icon, { FacebookIcon, InstagramIcon, TwitterIcon } from '../components/Icon'
 import { useState, useEffect } from 'react'
+import { OpenseaData } from '../interfaces/opensea'
+import Icon, { FacebookIcon, InstagramIcon, TwitterIcon } from '../components/Icon'
 import axios from 'axios'
-
-type OpenseaData = {
-  address: string
-  asset_contract_type: string
-  buyer_fee_basis_points: number
-  collection: {
-    banner_image_url: string
-    chat_url?: string
-    created_date: string
-    default_to_fiat: boolean
-    description: string
-    dev_buyer_fee_basis_points: string
-    dev_seller_fee_basis_points: string
-    discord_url?: string
-    display_data: {
-      card_display_style: string
-    }
-    external_url: string
-    featured: boolean
-    featured_image_url: string
-    hidden: boolean
-    image_url: string
-    instagram_username?: string
-    is_subject_to_whitelist: boolean
-    large_image_url: string
-    medium_username?: string
-    name: string
-    only_proxied_transfers: boolean
-    opensea_buyer_fee_basis_points: string
-    opensea_seller_fee_basis_points: string
-    payout_address: string
-    require_email: boolean
-    safelist_request_status: string
-    short_description?: string
-    slug: string
-    telegram_url?: string
-    twitter_username?: string
-    wiki_url?: string
-  }
-  created_date: string
-  default_to_fiat: boolean
-  description: string
-  dev_buyer_fee_basis_points: number
-  dev_seller_fee_basis_points: number
-  external_link: string
-  image_url: string
-  name: string
-  nft_version: string
-  only_proxied_transfers: boolean
-  opensea_buyer_fee_basis_points: number
-  opensea_seller_fee_basis_points: number
-  opensea_version?: string
-  owner?: string
-  payout_address: string
-  schema_name: string
-  seller_fee_basis_points: number
-  symbol: string
-  total_supply: string
-}
 
 const iconLists = [
 {img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1200px-Facebook_Logo_%282019%29.png", title: "Facebook" , connected: false},
@@ -156,7 +98,7 @@ const Page = () => {
       </div>
       <span className="text-left">Works(4)</span>
       <div className="grid grid-cols-2 gap-4 my-4 mb-8">
-        <a onClick={() => Router.push('/next_page')} className="style-box-primary artwork-card flex-col text-left">
+        <a onClick={() => Router.push('/collection?address=0x12f28e2106ce8fd8464885b80ea865e98b465149')} className="style-box-primary artwork-card flex-col text-left">
           <img className="m-auto block thumbnail-work" src={creator?.collection?.large_image_url} alt="" />
           <div className="mt-2">
             <span className="text-sm	block text-blue-600">Current Owner: Thanon</span>
