af6eba71396787a4cf7ef031258fb1f26d90dbc0
Chinnatip Taemkaeo <chin@kohlife.com>
2021-04-11 20:31:54 +0700

    masonry

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,3 +1,55 @@
+import Card from "../components/Card"
+
+const images = [
+  {
+    title: "Photo title",
+    img:
+      "https://images.unsplash.com/photo-1536431311719-398b6704d4cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    title: "Photo title",
+    img:
+      "https://images.unsplash.com/photo-1510797215324-95aa89f43c33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    title: "Photo title",
+    img:
+      "https://images.unsplash.com/photo-1543297031-d102cd432d54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    title: "Photo title",
+    img:
+      "https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    title: "Photo title",
+    img:
+      "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    title: "Photo title",
+    img:
+      "https://images.unsplash.com/photo-1500534623283-312aade485b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    title: "Photo title",
+    img:
+      "https://images.unsplash.com/reserve/HgZuGu3gSD6db21T3lxm_San%20Zenone.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    title: "Photo title",
+    img:
+      "https://images.unsplash.com/photo-1444464666168-49d633b86797?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  },
+  {
+    title: "Photo title",
+    img:
+      "https://images.unsplash.com/photo-1465146344425-f00d5f5c8f07?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+  }
+];
+
+
+
 const Navbar = () => {
   return <div className="flex items-center mb-10">
     <img className="h-10" src="image/nft_pass_logo.png" alt=""/>
@@ -115,16 +167,24 @@ const Page = () => {
 
       <div className="h-10"/>
 
-      <div className="rounded-24 mb-20 px-12 py-16 border border-white shadow-nft" style={{background: '#92878726'}}>
+      <div className="rounded-24 mb-20 px-12 py-16 border border-white shadow-nft" style={{background: '#ffffff8c'}}>
         <p className="text-lg mb-3">Collections</p>
-        <span className="bg-white rounded-full h-10 inline-flex px-1 pr-3 font-bold items-center">
+        <span className="bg-white shadow-nft rounded-full h-10 inline-flex px-1 pr-3 font-bold items-center">
           <img src="image/opensea_icon.png" className="h-8 inline mr-2"/> Opensea
         </span>
-        <span className="ml-3 bg-white rounded-full h-10 inline-flex px-1 pr-3 font-bold items-center">
+        <span className="ml-3 bg-white shadow-nft rounded-full h-10 inline-flex px-1 pr-3 font-bold items-center">
           <img src="image/rarible_icon.png" className="h-8 inline mr-2"/> Rarible
         </span>
 
         <p className="text-lg mt-8">Works</p>
+
+        <div className="masonry px-16 py-8">
+          {images.map(item => (
+            <div className="rounded-lg overflow-hidden mb-8" key={item.img}>
+              <Card src={item.img} title={item.title} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   </div>
