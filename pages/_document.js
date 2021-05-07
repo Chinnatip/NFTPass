8d4c2f801cc890457d205edaca546cc768f26fc5
Chinnatip Taemkaeo <chin@kohlife.com>
2021-05-07 20:43:34 +0700

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -8,12 +8,18 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
-          {/* Open Graph */}
+          <title>Galleryst</title>
           <meta property="og:url" content="https://galleryst.co/" key="ogurl" />
           <meta property="og:image" content="https://koh-assets.s3-ap-southeast-1.amazonaws.com/galleryst/ogimage.png" key="ogimage" />
           <meta property="og:site_name" content="Galleryst" key="ogsitename" />
           <meta property="og:title" content="Galleryst" key="ogtitle" />
           <meta property="og:description" content="Our initial idea is to gather all the NFTs artworks from all the platforms, so you will have a place where you can always get updated of all the NFTs art from your favourite artists. We don’t know how far can our site will go, but we are believing that NFTs will change the way people appreciate and exchange art forever. If you have any cool ideas about NFTs feel free to ping us, we would love to hear your thoughts" key="ogdesc" />
+          <link rel='shortcut icon' type='image/x-icon' href='/favicon/favicon.ico' />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="manifest" href="/favicon/manifest.json" />
+          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
