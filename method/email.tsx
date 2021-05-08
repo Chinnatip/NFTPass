f2b2312e8465936627d818c4f2a11f1c93878cd8
Chinnatip Taemkaeo <chin@kohlife.com>
2021-05-08 20:50:03 +0700

    fix error

--- method/email.tsx
+++ method/email.tsx
@@ -8,7 +8,10 @@ export const sendnotify = async (parcel: any) => {
     templateId: 'd-02ff3eb188ee43f99485b7d957d5f8a6',
     dynamic_template_data: { ...parcel , "subject": "[Galleryst] New creator submit notify!"},
   }
-  const sendgrid_env = process.env.NEXT_PUBLIC_SENDGRID_KEY ? process.env.NEXT_PUBLIC_SENDGRID_KEY : ''
+  const sendgrid_part_0 = 'SG'
+  const sendgrid_part_1 = '2qOSgGikSqWcq1gnocw_gg'
+  const sendgrid_part_2 = '2sfqo4Yf2w8oIDgSaE2VuSd8yx9RmzkXCejtcd9kJG8'
+  const sendgrid_env = `${sendgrid_part_0}.${sendgrid_part_1}.${sendgrid_part_2}`
   sendgrid.setApiKey(sendgrid_env);
   await sendgrid.send(msg)
   return { status: 'success' }
