ad53fd50ccbba1f2369cba7c9844f786d94a2c0b
Chinnatip Taemkaeo <chin@kohlife.com>
2021-07-14 17:31:18 +0700

    test og:image

--- pages/nft/index.tsx
+++ pages/nft/index.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Head from 'next/head'
 import { User, NFTDetail, ResponseDetail } from '../../interfaces/index'
 import * as rarible from '../../method/rarible/fetch'
 import * as opensea from '../../method/opensea/fetch'
@@ -109,10 +110,16 @@ const Page = ({ address }: { address: string }) => {
       }
     })()
   }, []);
-
   const {image, title, description, pricing, offer, creator, owner, activity } = nft
   const getDate = (dayFormat: string) => dayjs(dayFormat).format('DD MMM YYYY')
   return <div className="w-screen h-screen z-20 bg-white fixed top-0 left-0 overflow-y-scroll overflow-x-hidden">
+    <Head>
+      <meta property="og:image" content="https://api.placid.app/u/sxpwrxogf?&thumbnail[image]=https%3A%2F%2Flh3.googleusercontent.com%2Fs5yq1Hlr32j77n3KkHfhxkxoEX2I6Df78m_sU6L_136ZLFk8EM8ETdAFjWZNofkH2oT-dt1GpcdR4uWC14PPlzoCUy4MDOPzSnQGhA&title[text]=Gutter+Cat+%231409&creator_name[text]=Peterj.eth"/>
+      <meta property="og:image:height" content="600"/>
+      <meta property="og:image:width" content="1200"/>
+      <meta property="twitter:image" content="https://api.placid.app/u/sxpwrxogf?&thumbnail[image]=https%3A%2F%2Flh3.googleusercontent.com%2Fs5yq1Hlr32j77n3KkHfhxkxoEX2I6Df78m_sU6L_136ZLFk8EM8ETdAFjWZNofkH2oT-dt1GpcdR4uWC14PPlzoCUy4MDOPzSnQGhA&title[text]=Gutter+Cat+%231409&creator_name[text]=Peterj.eth"/>
+      <meta name="twitter:card" content="summary_large_image" />
+    </Head>
     <div className="flex flex-col">
       <div className="w-full relative flex items-center justify-center" style={{ background: 'rgba(92, 86, 86, 0.48)', height: '75vh' }}>
         <div className="p-4 flex items-center flex-col" style={{height: '100%'}}>
