92cab7c694dfa0daccdd59f1310e1a5bfa78a0ea
Chinnatip Taemkaeo <chin@kohlife.com>
2021-07-16 03:48:55 +0700

    fix replaceAll#5

--- pages/nft/index.tsx
+++ pages/nft/index.tsx
@@ -166,7 +166,7 @@ const checkDiff = (current_update: number, diffAmount: number = 2) => {
 }
 
 const prepareURI = (text: string) => {
-  const rep = text.split("#").join("@")
+  let rep = text.split("#").join("@").split("&").join("-").split("?").join("-")
   return encodeURI(rep)
 }
 
