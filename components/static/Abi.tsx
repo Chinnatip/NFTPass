a58fc910a5d085235087b7f4717e29757b9ff0c1
kaoths <tanawit139@gmail.com>
2021-07-20 16:22:18 +0700

    add standard detection

--- components/static/Abi.tsx
+++ components/static/Abi.tsx
@@ -1,21 +1,66 @@
-export const URI_ABI = [
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
-    name: 'uri',
-    outputs: [
-      {
-        internalType: 'string',
-        name: '',
-        type: 'string',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
+export const URI_ABI = {
+  inputs: [
+    {
+      internalType: 'uint256',
+      name: '',
+      type: 'uint256',
+    },
+  ],
+  name: 'uri',
+  outputs: [
+    {
+      internalType: 'string',
+      name: '',
+      type: 'string',
+    },
+  ],
+  stateMutability: 'view',
+  type: 'function',
+}
+
+export const SUPPORT_INTERFACE_ABI = {
+  inputs: [
+    {
+      internalType: 'bytes4',
+      name: 'interfaceId',
+      type: 'bytes4',
+    },
+  ],
+  name: 'supportsInterface',
+  outputs: [
+    {
+      internalType: 'bool',
+      name: '',
+      type: 'bool',
+    },
+  ],
+  stateMutability: 'view',
+  type: 'function',
+}
+
+export const TOKEN_URI_ABI = {
+  inputs: [
+    {
+      internalType: 'uint256',
+      name: 'tokenId',
+      type: 'uint256',
+    },
+  ],
+  name: 'tokenURI',
+  outputs: [
+    {
+      internalType: 'string',
+      name: '',
+      type: 'string',
+    },
+  ],
+  stateMutability: 'view',
+  type: 'function',
+}
+
+// prettier-ignore
+export const REQUIRED_ABIs = [
+  SUPPORT_INTERFACE_ABI, // ERC-165  -> supportsInterface(bytes4 interfaceID)
+  TOKEN_URI_ABI,         // ERC-721  -> tokenURI(uint256 _tokenId)
+  URI_ABI                // ERC-1155 -> uri(uint256 _id)
 ]
